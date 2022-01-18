c002aff2c95d359e1850bbf9ed4bf8d8649c4549
mct125 <mct125@scarletmail.rutgers.edu>
2022-01-18 11:06:03 -0500

    mobile links fix

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { isMobile } from 'react-device-detect';
 import me from '../images/me.png';
 import linkedin from '../images/linkedin.png';
 import email from '../images/email.png';
@@ -23,21 +24,37 @@ export default class About extends Component {
                             <h1>Hi, I'm Mary Tomkins</h1>
                             <h2>I am a software engineer, specializing in UI development, with a passion for graphic design.</h2>
                         </div>
+                        {isMobile && (
+                        <div className="contact-images">
+                            <a href="https://drive.google.com/file/d/1QD6eYjCbpMllfMDfXSlDvzWfLPhVOSHl/view?usp=sharing" target="_blank">
+                                <img src={resume} id="resume"></img>
+                            </a>
+                            <a href="mailto:maryctomkins@gmail.com" target="_blank">
+                                <img src={email} id="email"></img>
+                            </a>
+                            <a href="https://www.linkedin.com/in/mary-tomkins-90b37a174/" target="_blank">
+                                <img src={linkedin}></img>
+                            </a>
+                            <a href="https://github.com/marytomkins" target="_blank">
+                                <img src={github} id="git"></img>
+                            </a>
+                        </div>)}
                     </div>
-                    <div className="contact-images">
-                        <a href="https://drive.google.com/file/d/1QD6eYjCbpMllfMDfXSlDvzWfLPhVOSHl/view?usp=sharing" target="_blank">
-                            <img src={resume} id="resume"></img>
-                        </a>
-                        <a href="mailto:maryctomkins@gmail.com" target="_blank">
-                            <img src={email} id="email"></img>
-                        </a>
-                        <a href="https://www.linkedin.com/in/mary-tomkins-90b37a174/" target="_blank">
-                            <img src={linkedin}></img>
-                        </a>
-                        <a href="https://github.com/marytomkins" target="_blank">
-                            <img src={github} id="git"></img>
-                        </a>
-                    </div>
+                    {!isMobile && (
+                        <div className="contact-images">
+                            <a href="https://drive.google.com/file/d/1QD6eYjCbpMllfMDfXSlDvzWfLPhVOSHl/view?usp=sharing" target="_blank">
+                                <img src={resume} id="resume"></img>
+                            </a>
+                            <a href="mailto:maryctomkins@gmail.com" target="_blank">
+                                <img src={email} id="email"></img>
+                            </a>
+                            <a href="https://www.linkedin.com/in/mary-tomkins-90b37a174/" target="_blank">
+                                <img src={linkedin}></img>
+                            </a>
+                            <a href="https://github.com/marytomkins" target="_blank">
+                                <img src={github} id="git"></img>
+                            </a>
+                        </div>)}
                 </div>
             </div>
         )
